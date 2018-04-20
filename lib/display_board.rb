84e8eb28e51c57fdbd28c5e9197a5b3f014b9be2
Grace Shih <vocalise@gmail.com>
2018-04-20 23:03:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board
-  output = "   |   |   \n"
+  output =  " #{} | #{} | #{} \n"
   output += "-----------\n"
-  output += "   |   |   \n"
+  output += " #{} | #{} | #{} \n"
   output += "-----------\n"
-  output += "   |   |   \n"
+  output += " #{} | #{} | #{} \n"
   print output
 end
